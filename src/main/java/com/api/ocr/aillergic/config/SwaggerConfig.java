10eaa80481038314a3bd7375e5346e211d1b47bc
meuplow <gabr.menin@gmail.com>
2023-10-21 18:29:10 -0300

    fix: swagger config package

--- src/main/java/com/api/ocr/aillergic/config/SwaggerConfig.java
+++ src/main/java/com/api/ocr/aillergic/config/SwaggerConfig.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.api.ocr.aillergic;
+package com.api.ocr.aillergic.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
