88f3693887b3ba518fe22353e3b5e6a26f044d36
meuplow <gabr.menin@gmail.com>
2023-10-20 23:55:01 -0300

    start spring project

---
+++ src/main/java/com/api/ocr/aillergic/OcrRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.ocr.aillergic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcrRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcrRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/ocr/aillergic/OcrRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.ocr.aillergic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcrRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
